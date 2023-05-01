8c0f8ff5c5530f372ac5a1e6d4e18977d20ba43a
Gabriel <gabriel1412131718@gmail.com>
2023-04-30 22:30:27 -0300

    banco de dados
    configurações do banco de dados

--- worktec_tcc/lib/database/db.dart
+++ worktec_tcc/lib/database/db.dart
@@ -1,3 +1,5 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 class DB {
   //construtor privado
   DB._();
@@ -5,4 +7,84 @@ class DB {
   static final DB instance = DB._();
   //instancia do SQLite
   static Database? _database;
+
+  get database async {
+    if(_database != null) return _database;
+
+    return await _initDatabase();
+  }
+
+  _initDatabase() async{
+    return await openDatabase(
+      join(await getDatabasesPath(), 'worktec.db'),
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  _onCreate(db, versao) async {
+    await db.execute(_tb_curso);
+    await db.execute(_tb_materia);
+    await db.execute(_tb_orientador);
+    await db.execute(_tb_tcc);
+    await db.execute(_tb_aluno);
+  }
+
+  String get _tb_curso => ''' 
+    CREATE TABLE tb_curso (
+      id_curso TEXT PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
+    );
+  ''';
+
+  String get _tb_materia => '''
+    CREATE TABLE tb_materia(
+      id_materia TEXT PRIMARY KEY AUTOINCREMENT,
+      id_curso TEXT NOT NULL,
+      nome TEXT NOT NULL,
+      CONSTRAINT fk_id_curso FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso)
+    );
+ ''';
+
+  String get _tb_orientador=> '''
+    CREATE TABLE tb_professor(
+      id_professor TEXT PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL,
+      id_curso TEXT NOT NULL,
+      id_materia TEXT NOT NULL,
+      CONSTRAINT fk_id_curso FOREIGN KEY (id_curso) REFERENCES tb_curso(is_curso),
+      CONSTRAINT fk_id_materia FOREIGN KEY (id_materia) REFERENCES tb_materia(id_materia)
+    );
+ ''';
+
+  String get _tb_aluno=> '''
+    CREATE TABLE tb_aluno(
+      nome TEXT NOT NULL,
+      id_aluno TEXT PRIMARY KEY AUTOINCREMENT,
+      id_curso TEXT,
+      email TEXT,
+      CONSTRAINT fk_id_curso FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso)
+    );
+ ''';
+
+
+  String get _tb_tcc=> '''
+    CREATE TABLE tb_tcc(
+      id_tcc TEXT PRIMARY KEY AUTOINCREMENT,
+      id_orientador TEXT NOT NULL,
+      id_coorientador TEXT,
+      data TEXT NOT NULL,
+      id_aluno1 TEXT NOT NULL,
+      id_aluno2 TEXT,
+      id_aluno3 TEXT,
+      id_aluno4 TEXT,
+      CONSTRAINT fk_id_orientador FOREIGN KEY (id_orientador) REFERENCES tb_professor(id_professor),
+      CONSTRAINT fk_id_coorientador FOREIGN KEY (id_coorientador) REFERENCES tb_professor(id_professor),
+      CONSTRAINT fk_id_aluno1 FOREIGN KEY (id_aluno1) REFERENCES tb_aluno(id_aluno),
+      CONSTRAINT fk_id_aluno2 FOREIGN KEY (id_aluno2) REFERENCES tb_aluno(id_aluno),
+      CONSTRAINT fk_id_aluno3 FOREIGN KEY (id_aluno3) REFERENCES tb_aluno(id_aluno),
+      CONSTRAINT fk_id_aluno4 FOREIGN KEY (id_aluno4) REFERENCES tb_aluno(id_aluno)
+    );
+ ''';
+
 }
--- worktec_tcc/lib/src/config/models/lista_projetos.dart
+++ worktec_tcc/lib/src/config/models/lista_projetos.dart
@@ -1,18 +1,46 @@
 import 'dart:collection';
-
+import 'package:worktec_tcc/database/db.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:worktec_tcc/src/config/models/project_model.dart';
+
+import '../../../database/db.dart';
 
 class ListProjects extends ChangeNotifier{
 
+  late Database db;
   List<ObjectTcc>? _object = [];
 
   UnmodifiableListView<ObjectTcc> get lista => UnmodifiableListView(_object!);
 
-  void add(String nomeAluno, String nomeOrientador, String registroAluno, String registroOrientador, String tituloTrab, String palavraChave) { 
+
+  void add(String nomeAluno, String nomeOrientador, String registroAluno, String registroOrientador, String tituloTrab, String palavraChave) async{ 
       if(nomeAluno != '' && nomeOrientador != '' && registroAluno != '' && registroOrientador != '' && tituloTrab != '' && palavraChave != ''){
         _object!.add(ObjectTcc(nomeAluno: nomeAluno, nomeOrientador: nomeOrientador, 
                               registroAluno: registroAluno, registroOrientador: registroOrientador, tituloTrab: tituloTrab, palavraChave: palavraChave));
+      
+      /* 
+      newTcc(ObjectTcc newTcc) async {
+        final db = await DB.instance.database;
+        var res = await db.rawInsert(
+          "INSERT INTO tb_curso(nome)"
+          "VALUES (${'ADS'})"
+
+          "INSERT INTO tb_materia(id_curso, nome)"
+          "VALUES (${'1'}, ${'Programação em Microinformática'})"
+
+          "INSERT INTO tb_orientador(nome, id_curso, id_materia)"
+          "VALUES (${'1'}, ${newTcc.nomeOrientador}, ${'1'}, ${'1'})"
+
+          "INSERT INTO tb_aluno(nome, id_curso, email)"
+          "VALUES (${newTcc.nomeAluno}, ${'1'}, ${'exemplo@gmail.com'})"
+
+          "INSERT INTO tb_tcc(id_orientador, id_coorientador, data, id_aluno1)"
+          "VALUES (${'1'}, ${'1'}, ${'25/04/2023'}, ${'1'})"
+        );
+      }
+      */
+
      notifyListeners();
       }else{
         print("Não existem dados para a adição");
